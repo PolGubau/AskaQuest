faecebd0726b8c663b118327ce19feade592d76b
Pol Gubau <gubaupol@gmail.com>
2022-08-17 15:58:08 +0200

    bug: resporing prev login page

--- src/components/Layout/Forms/SignUpForm/SignUpForm.tsx
+++ src/components/Layout/Forms/SignUpForm/SignUpForm.tsx
@@ -1,3 +1,4 @@
+import email from "next-auth/providers/email";
 import router from "next/router";
 import React, { useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
@@ -9,41 +10,26 @@ import { messageSignUp } from "src/utils/text";
 import Swal from "sweetalert2";
 import styles from "./SignUpForm.module.css";
 
-import { Input, useInput } from "@nextui-org/react";
 
-//
+
+// 
 
 export default function SignUpForm() {
   const [userName, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+
   const [failedAllFields, setFailedAllFields] = useState(false);
+ 
   const [message, setMessage] = useState(messageSignUp.base);
-
-  const { value, reset, bindings } = useInput("");
-
-  const validateEmail = (value: string) => {
-    return value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
-  };
-
-  const helper = React.useMemo(() => {
-    if (!value)
-      return {
-        text: "",
-        color: "",
-      };
-    const isValid = validateEmail(value);
-    return {
-      text: isValid ? "Correct email" : "Enter a valid email",
-      color: isValid ? "success" : "error",
-    };
-  }, [value]);
-
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     // reset error and message
     setFailedAllFields(false);
+    setError("");
     setMessage("");
 
     // fields check
@@ -77,82 +63,71 @@ export default function SignUpForm() {
       router.push(PATH.SIGN_IN);
     } else {
       // set the error
-      return '404';
+      return setError(data.message);
     }
   };
   return (
-    <>
+    <form className={styles.formulario} onSubmit={handleSubmit}>
       {message && <p className={styles.message}>{message}</p>}
-      <form className={styles.formulario} onSubmit={handleSubmit}>
-        <div className={styles.inputBig}>
-          <div className={styles.inputIcon}>
-            <AiOutlineUser size={18} />
-          </div>
-          <Input
-            {...bindings}
-            clearable
-            shadow={false}
-            onClearClick={reset}
-            autoComplete="current-username"
-            onChange={(e) => setUserName(e.target.value)}
-            value={userName}
-            helperText={helper.text}
-            type="text"
-            labelPlaceholder="Username"
-            className={
-              failedAllFields && userName.length === 0
-                ? styles.inputError
-                : styles.input
-            }
-          />
-        </div>
-        <div className={styles.inputBig}>
-          <div className={styles.inputIcon}>
-            <HiOutlineMail size={18} />
-          </div>
-          <Input
-            {...bindings}
-            clearable
-            shadow={false}
-            onClearClick={reset}
-            autoComplete="current-email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            helperText={helper.text}
-            type="email"
-            labelPlaceholder="Email"
-            className={
-              failedAllFields && email.length === 0
-                ? styles.inputError
-                : styles.input
-            }
-          />
-        </div>
-        <div className={styles.inputBig}>
-          <div className={styles.inputIcon}>
-            <RiLockPasswordLine size={18} />
-          </div>
-          <input
-            type="password"
-            placeholder="Password"
-            autoComplete="current-password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            className={
-              failedAllFields && password.length === 0
-                ? styles.inputError
-                : styles.input
-            }
-          />
-        </div>
 
+      <div className={styles.inputBig}>
+        <div className={styles.inputIcon}>
+          <AiOutlineUser size={18} />
+        </div>
         <input
-          onClick={handleSubmit}
-          type={"submit"}
-          value="Create account"
-          className={styles.CreateAccountButton}
+          type="text"
+          placeholder="Username"
+          autoComplete="username"
+          onChange={(e) => setUserName(e.target.value)}
+          value={userName}
+          className={
+            failedAllFields && userName.length === 0
+              ? styles.inputError
+              : styles.input
+          }
         />
-      </form>
-    </>
+      </div>
+      <div className={styles.inputBig}>
+        <div className={styles.inputIcon}>
+          <HiOutlineMail size={18} />
+        </div>
+        <input
+          type="email"
+          placeholder="Email"
+          autoComplete="current-email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          className={
+            failedAllFields && email.length === 0
+              ? styles.inputError
+              : styles.input
+          }
+        />
+      </div>
+      <div className={styles.inputBig}>
+        <div className={styles.inputIcon}>
+          <RiLockPasswordLine size={18} />
+        </div>
+        <input
+          type="password"
+          placeholder="Password"
+          autoComplete="current-password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          className={
+            failedAllFields && password.length === 0
+              ? styles.inputError
+              : styles.input
+          }
+        />
+      </div>
+
+      <input
+        onClick={handleSubmit}
+        type={"submit"}
+        value="Create account"
+        className={styles.CreateAccountButton}
+      />
+    </form>
   );
 }
