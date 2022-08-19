84d4f1aec56722ba7c487b7c61f115565dc329fd
Pol Gubau <gubaupol@gmail.com>
2022-08-19 09:03:21 +0200

    bug: tags returned in array

--- src/components/Quests/EachQuest/EachQuest.js
+++ src/components/Quests/EachQuest/EachQuest.js
@@ -6,62 +6,67 @@ import StartButton from 'src/components/Buttons/StartButton/StartButton'
 import { useRouter } from 'next/router'
 import UserHeaderLoading from 'src/components/UserHeader/UserHeaderLoading'
 import { AiOutlineCalendar } from 'react-icons/ai'
+import { colors } from 'src/styles/theme'
+
 //
 export default function EachQuest({ collection }) {
   const router = useRouter()
 
-  const { ID, title, creator_id: creatorId, date_creation: dateCreation, likes, tags } = collection
-  console.log(tags, typeof tags)
-
+  const {
+    ID,
+    title,
+    creator_id: creatorId,
+    date_creation: dateCreation,
+    likes,
+    tags
+  } = collection
+  const tagsArray = JSON.parse(tags)
   const user = returnUserById(creatorId)
   return (
     <>
+      <section className={styles.section}>
+        {user
+          ? (
+          <UserHeader
+            username={user.userName || 'Anonymous'}
+            userimage={`https://api.multiavatar.com/${user.userName}.svg`}
+            likes={likes || null}
+          />
+            )
+          : (
+          <UserHeaderLoading />
+            )}
 
-        <section className={styles.section}>
-          {user
-            ? (
-            <UserHeader
-              username={user.userName || 'Anonymous'}
-              userimage={`https://api.multiavatar.com/${user.userName}.svg`}
-              likes={likes || null}
-            />
-              )
-            : (
-            <UserHeaderLoading />
-              )}
+        <div>
+          <h3 className={styles.title}>{title}</h3>
 
-          <div>
-            <h3 className={styles.title}>{title}</h3>
-
-            <p>
-              <span>
-
-                <AiOutlineCalendar />
-                {'Created '}
-                <TimeAgo datetime={dateCreation} locale="es.ts" />
-              </span>
-            </p>
-
-            {/* <div className={styles.tags}>
-              {tags &&
-                tags.map((tag, index) => (
-                  <span
-                    className={styles.tag}
-                    key={index}
-                    style={{
-                      color: colors.white,
-                      backgroundColor: colors.primary
-                    }}
-                  >
-                    {tag}
-                  </span>
-                ))}
-            </div> */}
+          <p>
+            <span>
+              <AiOutlineCalendar />
+              {'Created '}
+              <TimeAgo datetime={dateCreation} locale="es.ts" />
+            </span>
+          </p>
+          <div className={styles.tags}>
+            {tags &&
+              tagsArray.map((tag, index) => (
+                <span
+                  className={styles.tag}
+                  key={index}
+                  style={{
+                    color: colors.white,
+                    backgroundColor: colors.primary
+                  }}
+                >
+                  {tag}
+                </span>
+              ))}
           </div>
-          <div onClick={() => router.push('/Quest/[id]', `/Quest/${ID}`)}>
-            <StartButton />
-          </div>
-        </section>
+        </div>
+        <div onClick={() => router.push('/Quest/[id]', `/Quest/${ID}`)}>
+          <StartButton />
+        </div>
+      </section>
     </>
   )
 }
