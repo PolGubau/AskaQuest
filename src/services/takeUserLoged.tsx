da7fc465c47f24f4b0fca6587ead42501ce7962e
Pol Gubau <gubaupol@gmail.com>
2022-08-31 10:27:40 +0200

    deletting Service unused

--- src/services/takeUserLoged.tsx
+++
@@ -1,7 +0,0 @@
-import useLocalStorage from "src/hooks/getUserFromLocalStorage";
-
-export const takeUserLoged = () => {
-  const { con }: any = useLocalStorage("user");
-  if (!con) return "";
-  return con.data;
-};
