f7fdf32e2ebfc6fb05c0aaacdcb397533ce9b5ae
Pol Gubau <gubaupol@gmail.com>
2022-08-31 10:28:59 +0200

    Update UserHeader.tsx

--- src/components/userHeader/UserHeader.tsx
+++ src/components/userHeader/UserHeader.tsx
@@ -1,17 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./userHeader.module.css";
 import useLocalStorage from "src/hooks/getUserFromLocalStorage";
-import returnObjectById from "src/services/returnObjectById";
 import PATH from "src/utils/path";
+import UserInterface from "src/interfaces/User";
 
-export default function UserHeader({
+export default async function UserHeader({
   id = "",
   you = false,
   size = 40,
   name = "Anonymous",
   image = "https://api.multiavatar.com/Anonymous.svg",
+}: {
+  id: string;
+  you: boolean;
+  size: number;
+  name: string;
+  image: string;
 }) {
   if (you) {
     const {
@@ -22,10 +28,32 @@ export default function UserHeader({
   }
 
   if (id) {
-    const user = returnObjectById(PATH.API.USER_BY_ID, id);
-    const { data, status } = user;
-    name = data.userName;
-    image = data.image || `https://api.multiavatar.com/${name}.svg`;
+    const [userWithID, setUserWithID] = useState<UserInterface | undefined>(
+      undefined
+    );
+    const getUserByItsID = async () => {
+      const response = await fetch(`${PATH.API.USER_BY_ID}/${id}`).then(
+        (response) => response.json()
+      );
+
+      // update the state
+      setUserWithID(response);
+    };
+    useEffect(() => {
+      try {
+        getUserByItsID();
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+
+    if (userWithID) {
+      name = userWithID.userName;
+      image = userWithID.image || `https://api.multiavatar.com/${name}.svg`;
+    } else {
+      name = "Loading";
+      image = `https://api.multiavatar.com/loading.svg`;
+    }
   }
   return (
     <>
@@ -34,12 +62,18 @@ export default function UserHeader({
           <a className={styles.creator} style={{ height: size }}>
             <Image
               className={styles.creatorImage}
-              src={image || `https://api.multiavatar.com/${name}.svg`}
+              src={
+                image ||
+                `https://api.multiavatar.com/${name}.svg` ||
+                `https://api.multiavatar.com/Annonymous.svg`
+              }
               alt="Creator avatar"
               width={size - 10}
               height={size - 10}
             />
-            <span className={styles.creatorUserName}>{name}</span>
+            <span className={styles.creatorUserName}>
+              {name || "Loading..."}
+            </span>
           </a>
         </Link>
       </header>
