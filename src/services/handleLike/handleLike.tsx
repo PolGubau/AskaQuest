f0be0cd4fd499d269d631bee56ab18785f5ee3b8
Pol Gubau <gubaupol@gmail.com>
2022-08-31 10:28:30 +0200

    Update handleLike.tsx

--- src/services/handleLike/handleLike.tsx
+++ src/services/handleLike/handleLike.tsx
@@ -4,6 +4,7 @@ import { bigAlert, notificacionTop } from "src/utils/notifications";
 import PATH from "src/utils/path";
 import { updateCollection } from "../update/updateCollection";
 import { updateUser } from "../update/updateUser";
+import { updateLocalStorage } from "src/hooks/useLocalStorage";
 
 export const handleLike = (
   userLoged: UserInterface,
@@ -11,6 +12,15 @@ export const handleLike = (
   setLiked: any,
   setLikes: any
 ) => {
+  let { ID: CollectionID, likes: CollectionLikes } = collection;
+  if (!CollectionLikes) CollectionLikes = 0;
+  console.log("USER: ", userLoged);
+
+  let likedArray = userLoged.liked || [];
+
+  console.log(likedArray);
+
+  // handling errors
   if (userLoged.error) {
     bigAlert(
       "Oupss...",
@@ -19,16 +29,16 @@ export const handleLike = (
     );
     return;
   }
-
-  let { ID: CollectionID, likes: CollectionLikes } = collection;
-  if (!CollectionLikes) CollectionLikes = 0;
+  if (!CollectionID) {
+    bigAlert("Oupss...", `A problem happened here, please try again!`, "error");
+    return;
+  }
 
   let isNowLiked = 0;
 
   // TODO 1: update the user liked array in the database
   //1.1: check if the user liked the quest before
 
-  const likedArray = userLoged.liked;
   if (likedArray.includes(CollectionID)) {
     // the user already liked this collection, we need to remove it from the liked array
     const index = likedArray.indexOf(CollectionID);
@@ -38,12 +48,12 @@ export const handleLike = (
     isNowLiked = -1;
   } else {
     // the user didn't like this collection, we need to add it to the liked array
-    likedArray.push(collection.ID);
+    likedArray.push(CollectionID);
     setLiked(true);
     setLikes(CollectionLikes + 1);
     isNowLiked = 1;
   }
-  
+
   const newUser = { ...userLoged, liked: likedArray };
 
   // TODO 2: update the collection likes number in the database
@@ -52,7 +62,7 @@ export const handleLike = (
   const newCollection = { ...collection, likes: newLikes };
 
   // TODO 3: update the user in the localStorage
-  
+  // updateLocalStorage("user", newUser);
 
   updateUser(PATH.API.USER_BY_ID, newUser);
   updateCollection(PATH.API.COLLECTION_BY_ID, newCollection);
