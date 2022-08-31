769342226b369c30aca8dcb2940611748a04c175
Pol Gubau <gubaupol@gmail.com>
2022-08-31 10:28:50 +0200

    Update index.ts

--- src/pages/api/collections/index.ts
+++ src/pages/api/collections/index.ts
@@ -14,6 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       } catch (error) {
         return res.status(400).json({ error: error })
       }
+      break;
 
     case 'POST':
       try {
@@ -35,8 +36,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }   catch (error) {
         return res.status(400).json({ error, message: 'error', body })
       }
+      break;
 
     default:
       return res.status(404).json({ error: 'not found' })
+            break;
+
   }
 }
