b5fb7a6c1738a5c361926f5a5e7913f83e089030
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:30:40 +0200

    Update index.ts

--- src/pages/api/questions/index.ts
+++ src/pages/api/questions/index.ts
@@ -13,27 +13,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       } catch (error) {
         return res.status(400).json({ error })
       }
-      break;
 
     case 'POST':
-       try {
-        const { collection_id,title,solution,answers,creator_id} = JSON.parse(body)
+      try {
+        const { collection_id: collectionID, title, solution, answers, creator_id: creatorID } = JSON.parse(body)
         const answerJson = JSON.stringify(answers)
-       
-        const query = `INSERT INTO public."Questions" ("collection_id","title","solution","date_creation","answers","creatorID") VALUES ($1, $2, $3, $4, $5, $6);`
-        const values= [collection_id, title,solution, new Date(), answerJson,creator_id]
-        console.log('RECEIVED FROM BACK: ',values)
+
+        const query = 'INSERT INTO public."Questions" ("collection_id","title","solution","date_creation","answers","creatorID") VALUES ($1, $2, $3, $4, $5, $6);'
+        const values = [collectionID, title, solution, new Date(), answerJson, creatorID]
+        console.log('RECEIVED FROM BACK: ', values)
         const response = await conn.query(query, values)
 
-        return res.status(201).json({response, message: 'Question created' })
+        return res.status(201).json({ response, message: 'Question created' })
       } catch (error) {
         return res.status(400).json({ error })
       }
-      break;
 
     default:
       return res.status(404).json({ error: 'method not found' })
-            break;
-
   }
 }
