a9e3130f45734a1f816ec5e1b9fb377fec55a29b
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:31:04 +0200

    Update useRedirect.tsx

--- src/hooks/useRedirect.tsx
+++ src/hooks/useRedirect.tsx
@@ -1,8 +1,8 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-export default function useRedirect(where: string) {
-  const router = useRouter();
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+export default function useRedirect (where: string) {
+  const router = useRouter()
   useEffect(() => {
-    void router.push(where);
-  }, [router, where]);
+    void router.push(where)
+  }, [router, where])
 }
