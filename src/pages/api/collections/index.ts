b35b8f3f5b4f984e87c2126f509e4bbe2b32705b
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:30:48 +0200

    Update index.ts

--- src/pages/api/collections/index.ts
+++ src/pages/api/collections/index.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/space-before-function-paren */
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { Connection } from 'pg'
 import { conn } from 'src/utils/database'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -12,35 +11,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const response = await conn.query(query)
         return res.json(response.rows)
       } catch (error) {
-        return res.status(400).json({ error: error })
+        return res.status(400).json({ error })
       }
-      break;
 
     case 'POST':
       try {
         const content = JSON.parse(body)
-        
+
         const { creator_id: creatorID, title, tags } = content
-        
+
         const jsonTags = JSON.stringify(tags)
-        
+
         const query = `INSERT INTO public."Collections"( creator_id, date_creation,likes,title,tags)
           VALUES ($1, $2, $3,$4,$5) RETURNING *;`
-          
+
         const values = [creatorID, new Date(), 0, title, jsonTags]
-        
+
         const responsePOST = await conn.query(query, values)
         const collection = responsePOST.rows
 
         return res.status(200).json({ collection })
-      }   catch (error) {
+      } catch (error) {
         return res.status(400).json({ error, message: 'error', body })
       }
-      break;
 
     default:
       return res.status(404).json({ error: 'not found' })
-            break;
-
   }
 }
