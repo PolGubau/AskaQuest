cd1f1d5d4c3859add80318a6b132c9f82a52a128
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:31:00 +0200

    Update user.ts

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -9,8 +9,8 @@ export default interface UserInterface {
   image?: string
   followers?: Array<number | string> | any
   following?: Array<number | string> | any
-  collections_done?: any
-  liked: Array<number | string> 
-  error?:any
+  // collections_done: Array of: collection ID, percent of right answers, how many times the user has done the collection
+  collections_done?: Array<{id: number, per: number, times: number}>
+  liked: Array<number | string>
+  error?: any
 }
-
