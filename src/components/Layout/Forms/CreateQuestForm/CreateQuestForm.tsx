b1bb60a046674d725d2b0a6febadba522a76e493
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:32:30 +0200

    Update CreateQuestForm.tsx

--- src/components/Layout/Forms/CreateQuestForm/CreateQuestForm.tsx
+++ src/components/Layout/Forms/CreateQuestForm/CreateQuestForm.tsx
@@ -1,86 +1,86 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-var-requires */
-import React, { useEffect, useState } from "react";
-import { FaTextHeight } from "react-icons/fa";
-import styles from "./CreateQuestForm.module.css";
-import { messageCreateCollection } from "src/utils/text";
-import StartButton from "src/components/Buttons/StartButton/StartButton";
-import UserHeader from "src/components/UserHeader/UserHeader";
-import EachQuestionForm from "./EachQuestionForm/EachQuestionForm";
-import { QuestionInterface } from "src/interfaces/Question";
-import QuestionReaded from "./QuestionReaded/QuestionReaded";
-import getUserFromLocalStorage from "src/hooks/getUserFromLocalStorage";
-import PATH from "src/utils/path";
-import { useRouter } from "next/router";
-import UserInterface from "src/interfaces/User";
+import React, { useState } from 'react'
+import { FaTextHeight } from 'react-icons/fa'
+import styles from './CreateQuestForm.module.css'
+import { messageCreateCollection } from 'src/utils/text'
+import StartButton from 'src/components/Buttons/StartButton/StartButton'
+import UserHeader from 'src/components/UserHeader/UserHeader'
+import EachQuestionForm from './EachQuestionForm/EachQuestionForm'
+import { QuestionInterface } from 'src/interfaces/Question'
+import QuestionReaded from './QuestionReaded/QuestionReaded'
+import getUserFromLocalStorage from 'src/hooks/getUserFromLocalStorage'
+import PATH from 'src/utils/path'
+import { useRouter } from 'next/router'
+import UserInterface from 'src/interfaces/User'
 
-export default function CreateQuestForm() {
-  const router = useRouter();
-  const [questions, setQuestions] = useState<QuestionInterface[]>([]);
+export default function CreateQuestForm () {
+  const router = useRouter()
+  const [questions, setQuestions] = useState<QuestionInterface[]>([])
 
   const {
-    con: { status, user },
-  } = getUserFromLocalStorage();
+    con: { status, user }
+  } = getUserFromLocalStorage()
 
   // user will be undefined until status is 1
   if (status !== 1) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
   }
-  const { ID: creatorID } = user as UserInterface;
+  const { ID: creatorID } = user as UserInterface
 
-  const [title, setTitle] = useState("");
-  const [tags, setTags] = useState(["", "", ""]);
+  const [title, setTitle] = useState('')
+  const [tags, setTags] = useState(['', '', ''])
 
   const saveQuestions = (question: QuestionInterface) => {
-    if (user) question.creator_id = Number(creatorID);
-    question.date_creation = new Date();
-    setQuestions([...questions, question]);
-  };
-  const message = messageCreateCollection.base;
+    if (user) question.creator_id = Number(creatorID)
+    question.date_creation = new Date()
+    setQuestions([...questions, question])
+  }
+  const message = messageCreateCollection.base
 
   const handleSubmit = async (e: any) => {
-    e.preventDefault();
+    e.preventDefault()
 
     // 1. guardar la coleccion en la base de datos
     const collectionData = {
       creator_id: creatorID,
       likes: 0,
       title,
-      tags,
-    };
+      tags
+    }
     // save the post
     const response = await fetch(PATH.API.ALL_COLLECTIONS, {
-      method: "POST",
-      body: JSON.stringify(collectionData),
-    });
+      method: 'POST',
+      body: JSON.stringify(collectionData)
+    })
 
     // get the data
-    const data = await response.json();
+    const data = await response.json()
     // 2. coger el ID de la collection
-    const collectionID = data.collection[0].ID;
+    const collectionID = data.collection[0].ID
     // 3. guardar las preguntas en la base de datos una por una
     questions.forEach(async (question) => {
       const questionData = {
         ...question,
-        collection_id: collectionID,
-      };
-      console.log("SENDED BY FRONT: ", questionData);
+        collection_id: collectionID
+      }
+      console.log('SENDED BY FRONT: ', questionData)
 
       // save the post
       const response = await fetch(PATH.API.QUESTIONS, {
-        method: "POST",
-        body: JSON.stringify(questionData),
-      });
+        method: 'POST',
+        body: JSON.stringify(questionData)
+      })
 
       // get the data
-      const data = await response.json();
-      console.log("Result: ", data);
-    });
+      const data = await response.json()
+      console.log('Result: ', data)
+    })
 
     // 4. redirigir a la pagina de la collection
-    router.push(PATH.HOME);
-  };
+    router.push(PATH.HOME)
+  }
   return (
     <>
       {message && <p className={styles.message}>{message}</p>}
@@ -104,10 +104,10 @@ export default function CreateQuestForm() {
             you={true}
             searchById={{
               state: false,
-              id: "",
+              id: ''
             }}
-            name={""}
-            image={""}
+            name={''}
+            image={''}
           />
         </div>
         <section className={styles.tagsContainer}>
@@ -161,5 +161,5 @@ export default function CreateQuestForm() {
         </div>
       </form>
     </>
-  );
+  )
 }
