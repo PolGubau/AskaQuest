dc839c8c38ec046074eee4d7d49811ce520431dc
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:31:12 +0200

    Update UserHeader.tsx

--- src/components/userHeader/UserHeader.tsx
+++ src/components/userHeader/UserHeader.tsx
@@ -1,48 +1,47 @@
-import Image from "next/image";
-import Link from "next/link";
-import React, { useState } from "react";
-import styles from "./userHeader.module.css";
-import getUserFromLocalStorage from "src/hooks/getUserFromLocalStorage";
-import PATH from "src/utils/path";
-import UserInterface from "src/interfaces/User";
-import useFetch from "react-fetch-hook";
-import SquareLoader from "../loaders/SquaresLoader/SquareLoader";
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from './userHeader.module.css'
+import getUserFromLocalStorage from 'src/hooks/getUserFromLocalStorage'
+import PATH from 'src/utils/path'
+import UserInterface from 'src/interfaces/User'
+import useFetch from 'react-fetch-hook'
+import SquareLoader from '../loaders/SquaresLoader/SquareLoader'
 
-export default function UserHeader({
-  searchById = { state: false, id: "" },
+export default function UserHeader ({
+  searchById = { state: false, id: '' },
   you = false,
   size = 40,
-  name = "Anonymous",
-  image = "https://api.multiavatar.com/Anonymous.svg",
+  name = 'Anonymous',
+  image = 'https://api.multiavatar.com/Anonymous.svg'
 }: {
-  searchById: { state: boolean; id: string };
-  you: boolean;
-  size: number;
-  name: string;
-  image: string;
+  searchById: { state: boolean, id: string }
+  you: boolean
+  size: number
+  name: string
+  image: string
 }) {
-  let loading = false;
+  let loading = false
   if (you) {
     const {
-      con: { data },
-    }: any = getUserFromLocalStorage();
-    name = data?.userName;
-    image = data?.image;
+      con: { data }
+    }: any = getUserFromLocalStorage()
+    name = data?.userName
+    image = data?.image
   }
 
   if (searchById.state) {
-    const { isLoading, data }: { isLoading: boolean; data?: UserInterface } =
-      useFetch(`${PATH.API.USER_BY_ID}/${searchById.id}`);
+    const { isLoading, data }: { isLoading: boolean, data?: UserInterface } =
+      useFetch(`${PATH.API.USER_BY_ID}/${searchById.id}`)
 
     if (isLoading) {
-      loading = true;
+      loading = true
     }
-    if (data) {
-      name = data.userName;
-      image = data.image || `https://api.multiavatar.com/${name}.svg`;
+    if (data != null) {
+      name = data.userName
+      image = data.image ?? `https://api.multiavatar.com/${name}.svg`
     } else {
-      name = "Deletted User";
-      image = `https://api.multiavatar.com/Deletted.svg`;
+      name = 'Deletted User'
+      image = 'https://api.multiavatar.com/Deletted.svg'
     }
   }
   return (
@@ -50,16 +49,18 @@ export default function UserHeader({
       <header className={styles.container}>
         <Link href={`/profile/${name}`}>
           <a className={styles.creator} style={{ height: size }}>
-            {loading ? (
+            {loading
+              ? (
               <SquareLoader />
-            ) : (
+                )
+              : (
               <>
                 <Image
                   className={styles.creatorImage}
                   src={
                     image ||
                     `https://api.multiavatar.com/${name}.svg` ||
-                    `https://api.multiavatar.com/Annonymous.svg`
+                    'https://api.multiavatar.com/Annonymous.svg'
                   }
                   alt="Creator avatar"
                   width={size - 10}
@@ -67,10 +68,10 @@ export default function UserHeader({
                 />
                 <span className={styles.creatorUserName}>{name}</span>
               </>
-            )}
+                )}
           </a>
         </Link>
       </header>
     </>
-  );
+  )
 }
