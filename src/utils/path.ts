06d358ae47f2cffa5f9794ec256125308e7a15b8
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:29:21 +0200

    Update path.ts

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -8,28 +8,25 @@ const PATH = {
   PROFILE: `${DOMINIO}/profile`,
   ALL_QUESTS: `${DOMINIO}/allQuests`,
   CREATE_QUEST: `${DOMINIO}/createQuest`,
-  RANDOM_QUEST: `${DOMINIO}/Quest`,
+  QUEST: `${DOMINIO}/Quest`,
 
   // API ENDPOINTS
-  
-  API:{
+
+  API: {
     USERS: `${DOMINIO}/api/users`,
     USER_BY_ID: `${DOMINIO}/api/users/id`,
     USER_BY_USERNAME: `${DOMINIO}/api/users/userName`,
-    // 
+    //
     ALL_COLLECTIONS: `${DOMINIO}/api/collections`,
     COLLECTION_BY_ID: `${DOMINIO}/api/collections`,
     COLLECTION_BY_USERNAME: `${DOMINIO}/api/collections/userName`,
-    // 
+    //
     QUESTIONS: `${DOMINIO}/api/questions`,
-    QUESTIONS_MATCHING_COLLECTION: `${DOMINIO}/api/questions/MatchingByCollection`,
-},
+    QUESTIONS_MATCHING_COLLECTION: `${DOMINIO}/api/questions/MatchingByCollection`
+  },
   //
   GITHUB_LOGIN: `${DOMINIO}/api/auth/signin/github`,
   SIGN_OUT: `${DOMINIO}/api/auth/signout`
 }
-
-
-
 
 export default PATH
