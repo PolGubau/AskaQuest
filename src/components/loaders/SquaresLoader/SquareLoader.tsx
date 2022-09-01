62d7c44f91a885373175306cb5fc3cdf897f53c8
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:32:09 +0200

    Update SquareLoader.tsx

--- src/components/loaders/SquaresLoader/SquareLoader.tsx
+++ src/components/loaders/SquaresLoader/SquareLoader.tsx
@@ -1,25 +1,25 @@
-import styles from "./squaresLoader.module.css";
+import styles from './squaresLoader.module.css'
 
-export default function SquareLoader({
-  squaresColor = "#0270e1",
-  count = 3,
+export default function SquareLoader ({
+  squaresColor = '#0270e1',
+  count = 3
 }: any) {
   return (
     <>
       <div className={styles.loader}>
         <div
-          style={{ ["backgroundColor" as any]: squaresColor }}
+          style={{ ['backgroundColor' as any]: squaresColor }}
           className={styles.box}
         ></div>
         <div
-          style={{ ["backgroundColor" as any]: squaresColor }}
+          style={{ ['backgroundColor' as any]: squaresColor }}
           className={styles.box}
         ></div>
         <div
-          style={{ ["backgroundColor" as any]: squaresColor }}
+          style={{ ['backgroundColor' as any]: squaresColor }}
           className={styles.box}
         ></div>
       </div>
     </>
-  );
+  )
 }
