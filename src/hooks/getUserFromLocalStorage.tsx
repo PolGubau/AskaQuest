40dc0acfd44b0f03955f57e72ad5a31642e63073
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:31:08 +0200

    Update getUserFromLocalStorage.tsx

--- src/hooks/getUserFromLocalStorage.tsx
+++ src/hooks/getUserFromLocalStorage.tsx
@@ -1,41 +1,44 @@
-import { useEffect, useState } from "react";
-import UserInterface from "src/interfaces/User";
+import { useEffect, useState } from 'react'
+import UserInterface from 'src/interfaces/User'
 
-export default function getUserFromLocalStorage() {
+export default function getUserFromLocalStorage () {
   interface getUserFromLocalStorageInterface {
-    status: number;
-    user: UserInterface | undefined;
+    status: number
+    user: UserInterface | undefined
   }
   const [con, setCon] = useState<getUserFromLocalStorageInterface>({
     status: 0,
-    user: undefined,
-  });
+    user: undefined
+  })
 
   // we need to take the user from localstorage and return it with an status code
   useEffect(() => {
-    const user = localStorage.getItem("user");
+    const user = localStorage.getItem('user')
     if (user) {
-      const userRaw = JSON.parse(user);
-      const liked = JSON.parse(userRaw.liked);
-      const followers = JSON.parse(userRaw.followers);
-      const following = JSON.parse(userRaw.following);
+      const userRaw = JSON.parse(user)
+
+      const liked = JSON.parse(userRaw.liked)
+      const followers = JSON.parse(userRaw.followers)
+      const following = JSON.parse(userRaw.following)
+      const collections_done = JSON.parse(userRaw.collections_done)
 
       const newUser: UserInterface = {
         ...userRaw,
         followers,
         following,
         liked,
-      };
+        collections_done
+      }
       setCon({
         status: 1,
-        user: newUser,
-      });
+        user: newUser
+      })
     } else {
       setCon({
         status: -1,
-        user: undefined,
-      });
+        user: undefined
+      })
     }
-  }, []);
-  return { con };
+  }, [])
+  return { con }
 }
