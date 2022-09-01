09a7727ed211349544c9a006abf25b7cc7d0960c
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:29:32 +0200

    Update randomCollectionID.tsx

--- src/services/randomCollectionID.tsx
+++ src/services/randomCollectionID.tsx
@@ -1,22 +1,22 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-import PATH from "src/utils/path";
-import useSWR from "swr";
-import { offlineCollections } from "src/utils/offlineCollections";
+import PATH from 'src/utils/path'
+import useSWR from 'swr'
+import { offlineCollections } from 'src/utils/offlineCollections'
 const getData = async (url: string) => {
-  const response = await fetch(url);
-  return await response.json();
-};
+  const response = await fetch(url)
+  return await response.json()
+}
 
 const chooseOneFromArray = (array: any) => {
-  return array[Math.floor(Math.random() * array.length)];
-};
-export default function randomCollection() {
-  const allCollectionsEndpoint = `${PATH.API.ALL_COLLECTIONS}`;
+  return array[Math.floor(Math.random() * array.length)]
+}
+export default function randomCollection () {
+  const allCollectionsEndpoint = `${PATH.API.ALL_COLLECTIONS}`
   try {
-    const { data } = useSWR(allCollectionsEndpoint, getData);
-    return chooseOneFromArray(data);
+    const { data } = useSWR(allCollectionsEndpoint, getData)
+    return chooseOneFromArray(data)
   } catch {
-    const data = offlineCollections;
-    return chooseOneFromArray(data);
+    const data = offlineCollections
+    return chooseOneFromArray(data)
   }
 }
