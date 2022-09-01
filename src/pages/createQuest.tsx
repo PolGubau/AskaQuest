bb2cbeff0733ac3b21bcbaca26a555c95368cd1c
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:30:27 +0200

    Create createQuest.tsx

---
+++ src/pages/createQuest.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AppLayout from 'src/components/Layout/AppLayout'
+import CreateQuestForm from 'src/components/Layout/Forms/CreateQuestForm/CreateQuestForm'
+import Nav from 'src/components/Nav'
+
+export default function createQuest () {
+  return (
+    <>
+      <Nav />
+      <AppLayout>
+        <CreateQuestForm />
+      </AppLayout>
+    </>
+  )
+}
