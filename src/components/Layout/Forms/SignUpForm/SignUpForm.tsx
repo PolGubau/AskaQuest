38ebafa4bd6def1ba3ed23e63524606db351da1b
Pol Gubau <gubaupol@gmail.com>
2022-09-01 20:32:18 +0200

    Update SignUpForm.tsx

--- src/components/Layout/Forms/SignUpForm/SignUpForm.tsx
+++ src/components/Layout/Forms/SignUpForm/SignUpForm.tsx
@@ -1,89 +1,88 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-import router from "next/router";
-import React, { useState } from "react";
-import { AiOutlineUser } from "react-icons/ai";
-import { HiOutlineMail } from "react-icons/hi";
-import { RiLockPasswordLine } from "react-icons/ri";
-import StartButton from "src/components/Buttons/StartButton/StartButton";
-import { TopToastMessage } from "src/components/Messages/TopToastMessage";
-import { notificacionTop } from "src/utils/notifications";
-import PATH from "src/utils/path";
-import { messageSignUp } from "src/utils/text";
-import Swal from "sweetalert2";
-import styles from "./SignUpForm.module.css";
+import router from 'next/router'
+import React, { useState } from 'react'
+import { AiOutlineUser } from 'react-icons/ai'
+import { HiOutlineMail } from 'react-icons/hi'
+import { RiLockPasswordLine } from 'react-icons/ri'
+import StartButton from 'src/components/Buttons/StartButton/StartButton'
+import { TopToastMessage } from 'src/components/Messages/TopToastMessage'
+import PATH from 'src/utils/path'
+import { messageSignUp } from 'src/utils/text'
+import Swal from 'sweetalert2'
+import styles from './SignUpForm.module.css'
 
 //
 
-export default function SignUpForm() {
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+export default function SignUpForm () {
+  const [userName, setUserName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  const [error, setError] = useState("");
+  const [error, setError] = useState('')
 
-  const [failedAllFields, setFailedAllFields] = useState(false);
+  const [failedAllFields, setFailedAllFields] = useState(false)
 
-  const [message, setMessage] = useState(messageSignUp.base);
+  const [message, setMessage] = useState(messageSignUp.base)
 
   const handleSubmit = async (e: any) => {
-    e.preventDefault();
+    e.preventDefault()
 
     // reset error and message
-    setFailedAllFields(false);
-    setError("");
-    setMessage("");
+    setFailedAllFields(false)
+    setError('')
+    setMessage('')
 
     // fields check
     if (!userName || !email || !password) {
-      setFailedAllFields(true);
+      setFailedAllFields(true)
       return await Swal.fire(
-        "Ouups!",
+        'Ouups!',
         messageSignUp.allFieldsAreRequired,
-        "error"
-      );
+        'error'
+      )
     }
     // we need to check if this userName is already taken
-    const res = await fetch(`${PATH.API.USER_BY_USERNAME}/${userName}`);
-    const userIfIsTaken = await res.json();
-    console.log(userIfIsTaken);
+    const res = await fetch(`${PATH.API.USER_BY_USERNAME}/${userName}`)
+    const userIfIsTaken = await res.json()
+    console.log(userIfIsTaken)
     if (!userIfIsTaken.error) {
       return await Swal.fire(
-        "Ouups!",
-        "This userName is already taken",
-        "error"
-      );
+        'Ouups!',
+        'This userName is already taken',
+        'error'
+      )
     }
     // user structure
     const user = {
       userName,
       email,
       password,
-      image: `https://api.multiavatar.com/${userName}.svg`,
-    };
+      image: `https://api.multiavatar.com/${userName}.svg`
+    }
     // save the post
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify(user),
-    });
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify(user)
+    })
 
     // get the data
-    const data = await response.json();
+    const data = await response.json()
 
     if (data.success) {
-      setUserName("");
-      setEmail("");
-      setPassword("");
+      setUserName('')
+      setEmail('')
+      setPassword('')
       void TopToastMessage.fire({
-        icon: "success",
-        title: "Account Created! Now sign in with it",
-      });
-      void router.push(PATH.SIGN_IN);
+        icon: 'success',
+        title: 'Account Created! Now sign in with it'
+      })
+      void router.push(PATH.SIGN_IN)
     } else {
       // set the error
-      return setError(data.message);
+      return setError(data.message)
     }
-  };
+  }
   return (
     <form className={styles.formulario} onSubmit={handleSubmit}>
       {message && <p className={styles.message}>{message}</p>}
@@ -143,5 +142,5 @@ export default function SignUpForm() {
 
       <StartButton type="submit" text="Sign Up" fontSize="1.2rem" />
     </form>
-  );
+  )
 }
