e7f016d17b5c128d39863470cd2566d971cdc6ef
Pol Gubau <gubaupol@gmail.com>
2022-09-02 15:55:53 +0200

    Create index.tsx

---
+++ src/components/Nav/NavSettings/index.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import styles from './NavSettings.module.css'
+import { IoIosSettings } from 'react-icons/io'
+import { deleteUser } from 'src/services/delete/deleteUser'
+import { useRouter } from 'next/router'
+import PATH from 'src/utils/path'
+import { colors } from 'src/styles/theme'
+import { handleLogout } from 'src/services/handleLogin/handleLogout'
+
+function SettingsPannel ({
+  show = false,
+  changeShow,
+  userID,
+  userName = 'User'
+}: {
+  show: boolean
+  changeShow: Function
+  userID: number | string
+  userName: string
+}) {
+  const router = useRouter()
+
+  const handleSignOut = () => {
+    handleLogout()
+    void router.replace(PATH.SIGN_IN)
+  }
+  const handleDeleteUser = () => {
+    deleteUser(PATH.API.USER_BY_ID, userID)
+    localStorage.clear()
+    void router.push(PATH.HOME)
+  }
+  return (
+    <>
+      {show && (
+        <>
+          <div className={styles.container}>
+            <p className={styles.title}>Settings</p>
+            <button
+              className={styles.option}
+              onClick={async () => await router.push(PATH.HOME)}
+            >
+              Go Home
+            </button>
+            <button
+              className={styles.option}
+              onClick={async () => await router.push(PATH.PROFILE + `/${userName}`)}
+            >
+              Your Profile
+            </button>
+            <button className={styles.option} onClick={handleSignOut}>
+              Sign Out
+            </button>
+            <button className={styles.optionDanger} onClick={handleDeleteUser}>
+              Delete User
+            </button>
+          </div>
+
+          <div
+            className={styles.background}
+            onClick={() => {
+              changeShow()
+            }}
+          ></div>
+        </>
+      )}
+    </>
+  )
+}
+
+function NavSettings ({
+  userID,
+  userName
+}: {
+  userID: number | string
+  userName: string
+}) {
+  const [showSettings, setShowSettings] = useState(false)
+
+  const changeShow = () => setShowSettings(!showSettings)
+
+  return (
+    <>
+      <span
+        style={
+          showSettings
+            ? { backgroundColor: colors.background }
+            : { backgroundColor: colors.white }
+        }
+        className={`${styles.settingsButton} ${showSettings ? 'active' : ''}`}
+        onClick={changeShow}
+      >
+        <IoIosSettings className={styles.settingIcon} />
+      </span>
+      <SettingsPannel
+        userName={userName}
+        show={showSettings}
+        changeShow={changeShow}
+        userID={userID}
+      />
+    </>
+  )
+}
+
+export default NavSettings
