d53d0e467db9281c9525389d46a13aaa9c15ed35
Pol Gubau <gubaupol@gmail.com>
2022-09-02 15:58:33 +0200

    Create StartButton.js

---
+++ src/components/Buttons/StartButton/StartButton.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { FaArrowRight } from 'react-icons/fa'
+import styles from './StartButton.module.css'
+import SquareLoader from 'src/components/loaders/SquaresLoader/SquareLoader'
+
+//
+export default function StartButton ({
+  text = 'Start',
+  icon = 'arrow',
+  type = 'button',
+  outline = false,
+  fontSize = '1.5rem',
+  loadingProp = false
+}) {
+  const [loading, setLoading] = useState(loadingProp)
+
+  //
+  const handleClick = () => {
+    setLoading(true)
+  }
+  //
+  return (
+      <button
+        type={type}
+        style={{ outline: 'none', border: 'none', padding: 0 }}
+
+        onClick={handleClick}
+        className={`
+        ${styles.button} 
+        ${outline ? styles.outline : styles.noOutline}`}>
+
+          {!loading
+            ? (
+              <>
+                <p
+                  className={styles.startButtonText}
+                  style={{ fontSize }}
+                >
+                  {text}
+                </p>
+                <p className={styles.startButtonIcon}>
+                  {icon === 'arrow' && <FaArrowRight />}
+                </p>
+              </>
+              )
+            : (
+              <SquareLoader squaresColor="#fff" />
+              )}
+      </button>
+  )
+}
