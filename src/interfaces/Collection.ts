f6bc0dadf5e472a059a7b1dec6e77d9f048e4825
Pol Gubau <gubaupol@gmail.com>
2022-09-02 14:29:40 +0200

    Deletted all on beta

--- src/interfaces/Collection.ts
+++
@@ -1,9 +0,0 @@
-
-export interface CollectionInterface {
-  ID?: number
-  title: string
-  tags?: string
-  creator_id: number
-  date_creation?: string
-  likes?: number
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,10 @@
-import BodyComponent from 'src/components/BodyComponent/BodyComponent'
-import AppLayout from 'src/components/Layout/AppLayout'
-import Nav from 'src/components/Nav'
-import { CollectionInterface } from 'src/interfaces/Collection'
 
-import PATH from 'src/utils/path'
-
-export default function HomePage ({
-  collections
-}: {
-  collections: CollectionInterface[]
-}): JSX.Element {
+export default function HomePage (): JSX.Element {
   return (
     <>
-      <Nav navigatable={false}/>
-      <AppLayout>
         <main className="main">
           BETA
-          <BodyComponent collections={collections} />
         </main>
-      </AppLayout>
     </>
   )
-}
-
-export async function getStaticProps (): Promise<{
-  props: {
-    collections: CollectionInterface[]
-  }
-}> {
-  const collections = await fetch(PATH.API.ALL_COLLECTIONS).then(
-    async (res) => await res.json()
-  )
-  return { props: { collections } }
 }
