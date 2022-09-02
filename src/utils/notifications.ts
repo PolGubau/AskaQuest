8e9d5a3592e75e870eba5d9385c9c5cf9063f16f
Pol Gubau <gubaupol@gmail.com>
2022-09-02 15:34:43 +0200

    Create notifications.ts

---
+++ src/utils/notifications.ts
@@ -0,0 +1,28 @@
+import Swal, { SweetAlertIcon } from 'sweetalert2'
+
+export const topAlert = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+export const notificacionTop = (icon: SweetAlertIcon, title: string): void => {
+  topAlert.fire({ icon, title }).catch((err: Error) => console.log(err))
+}
+
+export const alertCenter = Swal.mixin({
+  position: 'center',
+  showConfirmButton: true,
+  showCancelButton: true,
+  confirmButtonText: 'Aceptar',
+  cancelButtonText: 'Cancelar',
+  reverseButtons: true
+})
+export const bigAlert = async (title: string, message: string, icon: SweetAlertIcon) => {
+  await Swal.fire(title, message, icon)
+}
