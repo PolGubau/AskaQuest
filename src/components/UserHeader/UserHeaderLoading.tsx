32a757ea1c806f298b202e65286e3a13dca176df
Pol Gubau <gubaupol@gmail.com>
2022-09-02 15:44:04 +0200

    Create UserHeaderLoading.tsx

---
+++ src/components/UserHeader/UserHeaderLoading.tsx
@@ -0,0 +1,19 @@
+import styles from './userHeader.module.css'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+export default function UserHeaderLoading () {
+  return (
+    <>
+      <header className={styles.container}>
+        <div className={styles.creator}>
+          <span className={styles.creatorUserName}>
+            <Skeleton className={styles.creatorImage} circle />
+          </span>
+          <Skeleton className={styles.userName} />
+        </div>
+
+      </header>
+    </>
+  )
+}
