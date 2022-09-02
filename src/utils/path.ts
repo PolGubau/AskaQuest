874bd41f19badb58e52bb7333dc3b82cc39e9382
Pol Gubau <gubaupol@gmail.com>
2022-09-02 15:34:40 +0200

    Create path.ts

---
+++ src/utils/path.ts
@@ -0,0 +1,33 @@
+
+const DOMINIO = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''
+
+const PATH = {
+  HOME: `${DOMINIO}`,
+  SIGN_IN: `${DOMINIO}/login`,
+  CREATE_ACCOUNT: `${DOMINIO}/signup`,
+  USER: `${DOMINIO}/profile`,
+  PROFILE: `${DOMINIO}/profile`,
+  ALL_QUESTS: `${DOMINIO}/allQuests`,
+  CREATE_QUEST: `${DOMINIO}/createQuest`,
+  QUEST: `${DOMINIO}/Quest`,
+
+  // API ENDPOINTS
+
+  API: {
+    USERS: `${DOMINIO}/api/users`,
+    USER_BY_ID: `${DOMINIO}/api/users/id`,
+    USER_BY_USERNAME: `${DOMINIO}/api/users/userName`,
+    //
+    ALL_COLLECTIONS: `${DOMINIO}/api/collections`,
+    COLLECTION_BY_ID: `${DOMINIO}/api/collections`,
+    COLLECTION_BY_USERNAME: `${DOMINIO}/api/collections/userName`,
+    //
+    QUESTIONS: `${DOMINIO}/api/questions`,
+    QUESTIONS_MATCHING_COLLECTION: `${DOMINIO}/api/questions/MatchingByCollection`
+  },
+  //
+  GITHUB_LOGIN: `${DOMINIO}/api/auth/signin/github`,
+  SIGN_OUT: `${DOMINIO}/api/auth/signout`
+}
+
+export default PATH
