13ffc369127b5899c4ca4982b4ac5ac37cd746ff
Pol Gubau <gubaupol@gmail.com>
2022-09-02 15:55:31 +0200

    Create index.js

---
+++ src/components/Question/ErrorNoQuestions/index.js
@@ -0,0 +1,23 @@
+
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import PATH from 'src/utils/path'
+import Swal from 'sweetalert2'
+
+export default function ErrorNoQuestion () {
+  const router = useRouter()
+  useEffect(() => {
+    Swal.fire({
+      title: 'Ouppps!',
+      text: 'This Collection has no questions',
+      icon: 'error',
+      confirmButtonText: 'Go Home'
+    }).finally(() => {
+      router.push(PATH.HOME)
+    })
+  }, [])
+
+  return (
+    <></>
+  )
+}
