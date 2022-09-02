edc9abd4a6a06f5c5255c085f795da1abd7acdb3
Pol Gubau <gubaupol@gmail.com>
2022-09-02 15:43:51 +0200

    Create user.ts

---
+++ src/interfaces/user.ts
@@ -0,0 +1,16 @@
+
+export default interface UserInterface {
+  ID: string
+  date_creation?: string
+  userName: string
+  password: string
+  email: string
+  role: number
+  image?: string
+  followers?: Array<number | string> | any
+  following?: Array<number | string> | any
+  // collections_done: Array of: collection ID, percent of right answers, how many times the user has done the collection
+  collections_done?: Array<{id: number, per: number, times: number}>
+  liked: Array<number | string>
+  error?: any
+}
