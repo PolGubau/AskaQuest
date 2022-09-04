6f9e17ae9579c87d2823a5ac5d9dd67a0339fe69
Pol Gubau <gubaupol@gmail.com>
2022-09-04 11:06:48 +0200

    Create signup.tsx

---
+++ src/pages/signup.tsx
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router'
+
+import AppLayout from 'src/components/Layout/AppLayout'
+import styles from 'src/styles/stylesPages/signup.module.css'
+import PATH from 'src/utils/path'
+import Logo from 'src/components/Logo/Logo'
+import SignUpForm from 'src/components/Layout/Forms/SignUpForm/SignUpForm'
+import Nav from 'src/components/Nav'
+
+// auth
+const SignUp = () => {
+  const router = useRouter()
+
+  return (
+    <>
+      <Nav seeProfile={false} />
+      <AppLayout>
+        <Logo />
+        <div className={styles.container}>
+          <section className={styles.singupSection}>
+            <SignUpForm />
+          </section>
+
+          <section className={styles.buttonSection}>
+            <div className={styles.buttonsSectionContainer}>
+              <p>Already having an account?</p>
+              <button
+                className={styles.SignInButton}
+                onClick={(e) => {
+                  router.push(PATH.SIGN_IN).catch((err) => console.log(err))
+                }}
+              >
+                Sign In
+              </button>
+            </div>
+          </section>
+        </div>
+      </AppLayout>
+    </>
+  )
+}
+
+export default SignUp
