b8da85e1b24383afd7d77bc38a47e56d0073f7b7
Pol Gubau <gubaupol@gmail.com>
2022-09-04 11:07:09 +0200

    Update SquareLoader.tsx

--- src/components/loaders/SquaresLoader/SquareLoader.tsx
+++ src/components/loaders/SquaresLoader/SquareLoader.tsx
@@ -1,25 +1,43 @@
 import styles from './squaresLoader.module.css'
+import styled from 'styled-components'
 
 export default function SquareLoader ({
   squaresColor = '#0270e1',
-  count = 3
-}: any) {
+  count = 3,
+  size = 30
+}: {
+  squaresColor?: string
+  count?: number
+  size?: number
+}) {
+  const Loader = styled.div`
+    size: ${size}px;
+    display: flex;
+    gap: 0.8rem;
+    align-content: center;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: nowrap;
+    flex-direction: row;
+`
+  const Box = styled.div`
+  border-radius: 10%;
+  width: ${size}px;
+  height: ${size}px;
+  animation: rotate 2s infinite;
+  background-color: ${squaresColor};
+`
   return (
     <>
-      <div className={styles.loader}>
-        <div
-          style={{ ['backgroundColor' as any]: squaresColor }}
+      <Loader>
+        {[...Array(count)].map((_, i) => (
+          <Box
+          key={i}
           className={styles.box}
-        ></div>
-        <div
-          style={{ ['backgroundColor' as any]: squaresColor }}
-          className={styles.box}
-        ></div>
-        <div
-          style={{ ['backgroundColor' as any]: squaresColor }}
-          className={styles.box}
-        ></div>
-      </div>
+        ></Box>
+        ))}
+
+      </Loader>
     </>
   )
 }
