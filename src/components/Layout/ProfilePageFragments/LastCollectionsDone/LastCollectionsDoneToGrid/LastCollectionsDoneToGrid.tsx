56af33ca6fab0afd8fab7dd38cd74d8c34ba14a5
Pol Gubau <gubaupol@gmail.com>
2022-09-04 13:50:18 +0200

    Update LastCollectionsDoneToGrid.tsx

--- src/components/Layout/ProfilePageFragments/LastCollectionsDone/LastCollectionsDoneToGrid/LastCollectionsDoneToGrid.tsx
+++ src/components/Layout/ProfilePageFragments/LastCollectionsDone/LastCollectionsDoneToGrid/LastCollectionsDoneToGrid.tsx
@@ -6,22 +6,29 @@ import returnObjectById from 'src/services/returnObjectById'
 import PATH from 'src/utils/path'
 import styles from './LastCollectionsDoneToGrid.module.css'
 export default function LastCollectionsDoneToGrid ({
-  collections, you, userLoged
+  collections, you = false, userLoged
 }:
 {collections: any, you: boolean, userLoged: UserInterface}) {
-  if (!collections || JSON.parse(collections).length === 0) {
-    return <p>This user has not done any collection...</p>
+  if (typeof collections === 'string') {
+    collections = JSON.parse(collections)
+  }
+  if (!collections || collections.length === 0) {
+    return (
+          <p>{you ? 'Complete a collection to save them here! ' : `${userLoged.userName} has not done any collection yet: `}</p>
+
+    )
   }
 
+  console.log(collections)
   return (
   <div className={styles.container}>
     <p>{you ? 'Your most done collections: ' : `${userLoged.userName}'s most done collections: `}</p>
     <div className={styles.containerCollection}>
 
-      {JSON.parse(collections).map((collectionID: {id: number, times: number, per: number }, index: number) => {
+      {collections.map((collectionID: {id: number, times: number, per: number }, index: number) => {
         const res = returnObjectById(PATH.API.ALL_COLLECTIONS, collectionID.id)
         if (res.status === 'loading') { return <SquareLoader size={20} key={`${collectionID.id}-loading-${index}`} /> }
-        return (<><EachQuest key={collectionID.id} collection={res.data} userLoged={userLoged} extraInfo={collectionID} /></>)
+        return (<><EachQuest key={`${collectionID.id}-charged-${index}`} collection={res.data} userLoged={userLoged} extraInfo={collectionID} /></>)
       })}
 
       </div>
