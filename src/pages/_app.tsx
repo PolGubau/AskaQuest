bbcae1ccaf678a7c99783a9c4543f79d34477ea6
Pol Gubau <gubaupol@gmail.com>
2022-09-06 14:44:18 +0200

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,19 @@ function MyApp ({ Component, pageProps }: AppProps) {
       <Head>
         <title>AskaQuest</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta name="author" content="Askaquest team"/>
+        <meta name="keywords" content="Nextjs, Questions, Answers"/>
+        <meta name="description" content="A free online app to display, create an post questions to let your friends answer them!"/>
+        <meta property="og:title" content="Askaquest" key="title" />
+        <meta property="og:site_name" content="Askaquest" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://askaquest.vercel.app/" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:description" content="A free online app to display, create an post questions to let your friends answer them!" />
+
       </Head>
       <Component {...pageProps} />
     </>
